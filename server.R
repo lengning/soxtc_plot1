f4288ebe6e5c3ff82470defa9eca3232aa446e2b
Ning Leng <lengning1@gmail.com>
2016-01-19 15:16:05 -0600

    both unlog and log

--- server.R
+++ server.R
@@ -14,9 +14,11 @@ names(day.col) <- sort(unique(day.ind))
 day.col.pool <- day.col[as.character(day.ind[cnames])]
 feeding.s <- rbind(c(0,0),c(21,24),c(37,40),c(69,72),c(96,96),c(141,144),c(189,192),c(237,240))
 	output$plot <- renderPlot({
-	par(mfrow=c(1,2))	
+	par(mfrow=c(1,3))	
 	plot(1, type="n", axes=F, xlab="", ylab="")
 	legend("top", paste("day", names(day.col)),col=day.col,ncol=4, lwd=2)
+	plot(data.norm[input$name,]-1,pch=20,col=day.col.pool, main=input$name,ylab="normalized expression")
+	for(i in 1:nrow(feeding.s))axis(3,feeding.s[i,],rep("",2),lwd=2,col="blue")
 	plot(data.norm[input$name,],pch=20,col=day.col.pool, main=input$name,ylab="normalized expression + 1 (log scale)",log="y")
 	for(i in 1:nrow(feeding.s))axis(3,feeding.s[i,],rep("",2),lwd=2,col="blue")
 	})
