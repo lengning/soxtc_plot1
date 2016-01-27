e2dbdc7469cdde4e0ff120fa8efd12c61a858c7e
Ning Leng <lengning1@gmail.com>
2016-01-27 10:23:37 -0600

    Update server.R

--- server.R
+++ server.R
@@ -16,7 +16,7 @@ feeding.s <- rbind(c(0,0),c(21,24),c(37,40),c(69,72),c(96,96),c(141,144),c(189,1
 	output$plot <- renderPlot({
 	par(mfrow=c(1,3),cex.lab=2, cex.axis=2, cex.main=2, mar=c(5,7,5,7))	
 	plot(1, type="n", axes=F, xlab="", ylab="")
-	legend("top", paste("day", names(day.col)),col=day.col,ncol=4, lwd=2,cex=2)
+	legend("top", paste("day", names(day.col)),col=day.col,ncol=3, lwd=2,cex=2)
 	plot(data.norm[input$name,]-1,pch=20,col=day.col.pool, main=input$name,ylab="normalized expression")
 	for(i in 1:nrow(feeding.s))axis(3,feeding.s[i,],rep("",2),lwd=4,col="blue")
 	plot(data.norm[input$name,],pch=20,col=day.col.pool, main=input$name,ylab="normalized expression + 1 (log scale)",log="y")
