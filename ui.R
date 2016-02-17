abb2e7c5b59b5398fc8c125428e736024966a592
Ning Leng <lengning1@gmail.com>
2016-02-17 10:21:22 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,5 +1,16 @@
 ui <- bootstrapPage(
-										  textInput('name', 'gene name', "T"),
-											  plotOutput('plot')
-											)
+ sidebarLayout(
+    sidebarPanel(
+      textInput('name', 'gene name', "T"),
+  #    sliderInput("ylims",
+   #               "y axis limits",
+   #               min = input$min,
+   #               max = input$max,
+   #               value = c(input$min, input$max))
+    ),
+
+    mainPanel(
+     plotOutput('plot')
+))
+)
 
