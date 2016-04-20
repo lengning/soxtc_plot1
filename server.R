12485e533c7e7727ba65436c823d3dc700c5fcc3
Ning Leng <lengning1@gmail.com>
2016-04-20 17:20:01 -0500

    Update server.R

--- server.R
+++ server.R
@@ -13,7 +13,7 @@ names(day.col) <- sort(unique(day.ind))
 
 day.col.pool <- day.col[as.character(day.ind[cnames])]
 feeding.s <- rbind(c(0,0),c(21,24),c(37,40),c(69,72),c(96,96),c(141,144),c(189,192),c(237,240))
-x.which <- which(h.ind%in%c(1,50,100,150,200))
+x.which <- which(h.ind%in%c(1,50,100,150,200,288))
 feeding.which <- feeding.s
 #for(i in 1:nrow(feeding.s))for(j in 1:2)feeding.which[i,j] <- which(h.ind==feeding.s[i,j])
 # dont show the last feeding interval ... no data available
@@ -26,11 +26,11 @@ for(i in 1:nrow(feeding.which))for(j in 1:2)feeding.which[i,j] <- which(h.ind==f
 	plot(1, type="n", axes=F, xlab="", ylab="")
 	legend("top", paste("day", names(day.col)),col=day.col,ncol=3, lwd=2,cex=2)
 	plot(data.norm[input$name,]-1,pch=20,col=day.col.pool, main=input$name,ylab="normalized expression", xlab="hr",xaxt="n")
-	axis(1,x.which,c(1,50,100,150,200))
+	axis(1,x.which,c(1,50,100,150,200,288))
 	for(i in 1:nrow(feeding.which))axis(3,feeding.which[i,],rep("",2),lwd=8,col="blue",col.ticks="white", lwd.ticks=.1)
 	plot(data.norm[input$name,],pch=20,col=day.col.pool, main=input$name,ylab="normalized expression + 1 (log scale)",
 	log="y",xlab="hr",xaxt="n")
-	axis(1,x.which,c(1,50,100,150,200))
+	axis(1,x.which,c(1,50,100,150,200,288))
 	for(i in 1:nrow(feeding.which))axis(3,feeding.which[i,],rep("",2),lwd=8,col="blue", col.ticks="white", lwd.ticks=.1)
 	})
 }
